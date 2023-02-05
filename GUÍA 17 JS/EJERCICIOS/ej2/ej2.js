0d1b6fab5e40ba6f406f09e781fd58c3cf6e63c4
mica <94196499+mickani@users.noreply.github.com>
2023-02-05 00:32:01 -0300

    Delete GUÍA 17 JS directory

--- GUÍA 17 JS/EJERCICIOS/ej2/ej2.js
+++
@@ -1,7 +0,0 @@
-function calcular() {
-    var radio = document.getElementById("radio").value;
-    var perimetro =Math.round((2 * Math.PI * radio));
-    var area =Math.round(Math.PI * (radio * radio));
-    document.getElementById("perimetro").innerHTML = "Perímetro: " + perimetro;
-    document.getElementById("area").innerHTML = "Área: " + area;
-}
--- GUÍA 17 JS/EJERCICIOS/ej9/ej9.js
+++
@@ -1,6 +0,0 @@
-function espaciarLetras() {
-  var letrasSeparadas = document.getElementById("frase").value.split('').join(" ");
-  document.getElementById("fraseConEspacio").innerHTML = letrasSeparadas;
-}
-
-document.getElementById("btnListo").onclick=espaciarLetras;
