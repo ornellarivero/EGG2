dcf5180d79e291c4e142959387ef043d4b28dbe3
mica <94196499+mickani@users.noreply.github.com>
2023-02-05 00:31:40 -0300

    Delete GUÍA 15 SpringBoot directory

--- GUÍA 15 SpringBoot/EJERCICIOS/EJ PRUEBA/biblioteca/biblioteca/src/main/java/com/egg/biblioteca/repositorios/AutorRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.egg.biblioteca.repositorios;
-
-import com.egg.biblioteca.entidades.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutorRepositorio extends JpaRepository<Autor, String>{
-
-}
